0eae83ca1bc5a0d966248a56bbc5a4c1bba413b9
Nishant Jain <nishantkumarajain@gmail.com>
2018-07-03 19:40:56 +0530

    Added lint rules

--- test/convert-english-to-braille-code.test.js
+++ test/convert-english-to-braille-code.test.js
@@ -1,14 +1,16 @@
 const should = require('should');
 const puppeteer = require('puppeteer');
 const StaticServer = require('static-server');
-
 const buynan = require('bunyan');
 const bFormat = require('bunyan-format');
+
 const formatOut = bFormat({ outputMode: 'short' });
 const log = buynan.createLogger({ name: 'Test', stream: formatOut, level: 'debug' });
 
 
-let page, browser, server;
+let page;
+let browser;
+let server;
 
 describe('English to Braille Converter', () => {
   before(async () => {
@@ -32,30 +34,26 @@ describe('English to Braille Converter', () => {
 
   it('Should have all the required elements', async () => {
     log.debug('Checking if all the elements are present');
-    const sourceLangElement = await page.evaluate(() =>
-      document.getElementById('sourceLangText'));
-    const btnConvertEnglishToBrailleElement = await page.evaluate(() =>
-      document.getElementById('btnConvertEnglishToBraille'));
-    const targetLangTextElement = await page.evaluate(() =>
-      document.getElementById('targetLangText'));
+    const sourceLangElement = await page.evaluate(() => document.getElementById('sourceLangText'));
+    const btnConvertEnglishToBrailleElement = await page.evaluate(() => document.getElementById('btnConvertEnglishToBraille'));
+    const targetLangTextElement = await page.evaluate(() => document.getElementById('targetLangText'));
 
-    should.exist(sourceLangElement, "The sourceLangElement is removed. Seems you have modified the index.html");
-    should.exist(btnConvertEnglishToBrailleElement, "The button with id `btnConvertEnglishToBraille` is removed. Seems you have modified index.html")
-    should.exist(targetLangTextElement, "The targetLangText element is removed. Seems you have modified the index.html");
+    should.exist(sourceLangElement, 'The sourceLangElement is removed. Seems you have modified the index.html');
+    should.exist(btnConvertEnglishToBrailleElement, 'The button with id `btnConvertEnglishToBraille` is removed. Seems you have modified index.html');
+    should.exist(targetLangTextElement, 'The targetLangText element is removed. Seems you have modified the index.html');
   });
 
   it('Should be able convert from English to Braille', async () => {
     // Types text and triggers click event to convert english to braille
     log.debug('Puppeteer typing the text and triggering the button click');
     await page.evaluate(() => {
-      document.getElementById('sourceLangText').value = "Hello World";
+      document.getElementById('sourceLangText').value = 'Hello World';
       document.getElementById('btnConvertEnglishToBraille').click();
     });
 
     // Fetches the converted text
     log.debug('Fetch the converted text from the target element');
-    const targetLangText = await page.evaluate(() =>
-      document.getElementById('targetLangText').innerHTML);
+    const targetLangText = await page.evaluate(() => document.getElementById('targetLangText').innerHTML);
 
     targetLangText.should.not.be.null();
     targetLangText.should.be.an.instanceOf(String);
